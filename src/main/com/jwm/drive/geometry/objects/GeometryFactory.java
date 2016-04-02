f8142e56a0511dc2cd7699e42f4583b04c4ba7d6
Jeff Mair <jeffwmair@gmail.com>
2016-04-02 13:59:07 -0400

    More class movements for better encapsulation

--- src/main/com/jwm/drive/geometry/objects/GeometryFactory.java
+++ src/main/com/jwm/drive/geometry/objects/GeometryFactory.java
@@ -3,6 +3,7 @@ package com.jwm.drive.geometry.objects;
 import com.jwm.j3dfw.geometry.Geometry;
 
 /**
+ * TODO: maybe put into j3dfw?
  * Created by Jeff on 2016-03-30.
  */
 public interface GeometryFactory {
